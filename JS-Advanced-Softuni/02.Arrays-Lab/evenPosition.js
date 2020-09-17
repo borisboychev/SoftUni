94486f39fa3c79bf2633cfb995ced992175a9b95
BorisBBoychev <borisboychev007@Gmail.com>
2020-09-17 19:39:59 +0300

    second exercise

--- JS-Advanced-Softuni/02.Arrays-Lab/evenPosition.js
+++ JS-Advanced-Softuni/02.Arrays-Lab/evenPosition.js
@@ -1,14 +1,11 @@
 function solve(arr) {
-  let el = "";
+  let result = [];
   for (let i = 0; i < arr.length; i++) {
     if (i % 2 == 0) {
-      el += arr[i] + " ";
+      result.push(arr[i]);
     }
   }
-  console.log(el.trim());
+  console.log(result.join(" "));
 }
 
 solve([20, 30, 40]);
-
-let a = 12 * 3;
-let b = 12 * 3;
---
+++ JS-Advanced-Softuni/02.Arrays-Lab/negativePositive.js
@@ -0,0 +1,6 @@
+function solve(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+    }
+  }
+}
