1840ee3373ea9d8ac21cec38ecee6430fdbb246e
BorisBBoychev <borisboychev007@Gmail.com>
2020-09-24 20:09:18 +0300

    first exercise

--- JS-Advanced-Softuni/03.Objects-and-classes-Lab]/townsToJson.js
+++ JS-Advanced-Softuni/03.Objects-and-classes-Lab]/townsToJson.js
@@ -1,3 +1,28 @@
-let person = { firstname: "John", lastName: "Doe", age: "21" };
+function solve(input) {
+  let data = input.map((row) =>
+    row
+      .split("|")
+      .filter((x) => x != "")
+      .map((x) => x.trim())
+  );
+  let properties = data.shift();
 
-console.log(person.age);
+  let result = [];
+
+  data.forEach((row) => {
+    let town = {
+      Town: row[0],
+      Latitiude: Number(Number(row[1]).toFixed(2)),
+      Longitude: Number(Number(row[2]).toFixed(2)),
+    };
+    result.push(town);
+  });
+
+  console.log(result);
+}
+
+solve([
+  "| Town | Latitude | Longitude |",
+  "| Sofia | 42.696552 | 23.32601 |",
+  "| Beijing | 39.913818 | 116.363625 |",
+]);
