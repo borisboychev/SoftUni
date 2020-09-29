039336116dd3ae6ad3b3e8100090eb0bf2892bd7
BorisBBoychev <borisboychev007@Gmail.com>
2020-09-29 23:46:01 +0300

    workshop part 1

---
+++ Python-Web-Basics-Softuni/petstagram/common/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def landing_page(request):
+    return render(request, 'index.html')
+
+
+def original(request):
+    return render(request, 'landing_page.html')
