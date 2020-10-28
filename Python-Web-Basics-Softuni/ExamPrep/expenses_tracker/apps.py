869b3472fa891087b0cf855a3d7d92c049e2880d
BorisBBoychev <borisboychev007@Gmail.com>
2020-10-28 16:26:14 +0200

    removed exam prep and created new exam prep

---
+++ Python-Web-Basics-Softuni/ExamPrep/expenses_tracker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExpensesTrackerConfig(AppConfig):
+    name = 'expenses_tracker'
--- Python-Web-Basics-Softuni/expenses_tracker/expenses_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from expenses_app.models import Expenses, Profile
-
-admin.site.register(Expenses)
-admin.site.register(Profile)
