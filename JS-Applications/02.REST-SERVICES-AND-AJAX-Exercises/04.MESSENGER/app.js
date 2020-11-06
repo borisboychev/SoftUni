f09c45373d66a486c0f9d6bacdd71cca6f38b1f6
BorisBBoychev <borisboychev007@Gmail.com>
2020-11-06 19:39:04 +0200

    exercise 4

--- JS-Applications/02.REST-SERVICES-AND-AJAX-Exercises/04.MESSENGER/app.js
+++ JS-Applications/02.REST-SERVICES-AND-AJAX-Exercises/04.MESSENGER/app.js
@@ -1,5 +1,33 @@
 function attachEvents() {
-    console.log('TODO...');
+  const url = "https://rest-messanger.firebaseio.com/messanger.json";
+  let btnSubmit = document.getElementById("submit");
+  let btnRefresh = document.getElementById("refresh");
+  let messagesTextarea = document.getElementById("messages");
+
+  btnRefresh.addEventListener("click", () => refresh(url, messagesTextarea));
+  btnSubmit.addEventListener("click", () => submit(url));
+
+  function refresh(url, textarea) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        let result = Object.values(data).reduce(
+          (messages, message) =>
+            (messages += `${message.author}: ${message.content}\n`)
+        );
+
+        textarea.textContent = result;
+      });
+  }
+
+  function submit() {
+    let author = document.getElementById("author");
+    let content = document.getElementById("content");
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ author: author.value, content: content.value }),
+    });
+  }
 }
 
 attachEvents();
