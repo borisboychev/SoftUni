10025540abd61db1daffa2e73da26d02eed7aaad
BorisBBoychev <borisboychev007@Gmail.com>
2020-11-06 18:19:30 +0200

    exercise 2

--- JS-Applications/02.REST-SERVICES-AND-AJAX-Exercises/02.BUS-SCHEDULE/app.js
+++ JS-Applications/02.REST-SERVICES-AND-AJAX-Exercises/02.BUS-SCHEDULE/app.js
@@ -1,17 +1,46 @@
 function solve() {
+  let departButton = document.getElementById("depart");
+  let arriveButton = document.getElementById("arrive");
+  let info = document.getElementById("info");
+  let stopId = "depot";
+  let stopName;
+  const baseUrl = `https://judgetests.firebaseio.com/schedule/`;
 
-    function depart() {
-        console.log('Depart TODO...');
+  function changeButton() {
+    if (departButton.disabled) {
+      departButton.disabled = false;
+      arriveButton.disabled = true;
+    } else {
+      departButton.disabled = true;
+      arriveButton.disabled = false;
     }
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function depart() {
+    const url = `${baseUrl}${stopId}.json`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        info.textContent = "Next stop " + data.name;
+        stopId = data.next;
+        stopName = data.name;
+      })
+      .catch(() => {
+        info.textContent = "ERROR";
+      });
 
-    return {
-        depart,
-        arrive
-    };
+    changeButton();
+  }
+
+  function arrive() {
+    info.textContent = "Arriving at " + stopName;
+    changeButton();
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
