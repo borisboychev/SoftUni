22062312f08e29a1d85377a6d41ef74966d37e99
BorisBBoychev <borisboychev007@Gmail.com>
2021-03-26 11:14:25 +0200

    exercise 1 and 2

--- C++-Fundamentals/01.Basic Syntax Exercises/Exercises/01.OrderTwoNumbers/main.cpp
+++ C++-Fundamentals/01.Basic Syntax Exercises/Exercises/01.OrderTwoNumbers/main.cpp
@@ -2,5 +2,12 @@
 
 int main() {
 
+	int a, b;
+
+	std::cin >> a >> b;
+
+	if (a > b) std::cout << b << " " << a << std::endl;
+	else std::cout << a << " " << b << std::endl;
+
 	return 0;
 }
--- C++-Fundamentals/01.Basic Syntax Exercises/Exercises/02.ProductSign/main.cpp
+++ C++-Fundamentals/01.Basic Syntax Exercises/Exercises/02.ProductSign/main.cpp
@@ -2,5 +2,14 @@
 
 int main() {
 
+	int a, b, c;
+
+	std::cin >> a >> b >> c;
+
+	int res = a * b * c;
+
+	if (res >= 0) std::cout << '+' << std::endl;
+	else std::cout << '-' << std::endl;
+
 	return 0;
 }
